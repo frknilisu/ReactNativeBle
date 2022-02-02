8fb47b9b70c954927eeb0a763749dc25d9123521
frknilisu <frkn.ilisu@gmail.com>
2022-02-02 09:29:02 +0100

    add subscribing notify characherestic func.

--- App.tsx
+++ App.tsx
@@ -32,13 +32,14 @@ import {
 LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
-import { BleManager, Device } from 'react-native-ble-plx';
+import { BleManager, Characteristic, Device } from 'react-native-ble-plx';
 import base64 from 'react-native-base64';
 
 const App = () => {
   
   const bleManager: BleManager = new BleManager();
   const [myDevice, setMyDevice] = useState<Device>();
+  const [notifChar, setNotifChar] = useState<Characteristic>();
   
   const SERVICE_UUID = "a5b1194c-0d11-e0c0-50e5-c7db856efe37";
   const CHAR_UUID= "98234604-4499-8e00-79d3-44a0c5481593";
@@ -66,8 +67,8 @@ const App = () => {
   }
 
   const connectToDevice = () => {
-    console.log("Device To Connect: ", myDevice.id);
-    bleManager.connectToDevice(myDevice.id)
+    console.log("Device To Connect: ", myDevice?.id);
+    bleManager.connectToDevice(myDevice?.id)
       .then((device) => {
         console.log("Connected: ", device.isConnected());
         console.log(device);
@@ -95,9 +96,9 @@ const App = () => {
   }
 
   const writeToDevice = (data: any) => {
-    console.log("Is Connected: ", myDevice.isConnected());
+    console.log("Is Connected: ", myDevice?.isConnected());
     bleManager.writeCharacteristicWithResponseForDevice(
-      myDevice.id, SERVICE_UUID, CHAR_UUID, base64.encode(data).toString('utf-8')
+      myDevice?.id, SERVICE_UUID, CHAR_UUID, base64.encode(data).toString('utf-8')
     ).then(response => {
       console.log(response);
     }).catch((error) => {
@@ -106,13 +107,31 @@ const App = () => {
   }
 
   const readFromDevice = () => {
-    console.log("Is Connected: ", myDevice.isConnected());
+    console.log("Is Connected: ", myDevice?.isConnected());
     bleManager.readCharacteristicForDevice(
-      myDevice.id, SERVICE_UUID, CHAR_UUID
+      myDevice?.id, SERVICE_UUID, CHAR_UUID
     ).then((char) => {
-      console.log(base64.decode(char.value));
+      console.log(base64.decode(char?.value));
     }).catch((error) => {
       console.log('error in reading data: ', error);
+    });
+  }
+
+  const subscribeToDevice = () => {
+    console.log("Is Connected: ", myDevice?.isConnected());
+    bleManager.monitorCharacteristicForDevice(myDevice?.id, SERVICE_UUID, CHAR_UUID, (error, characteristic) => {
+      console.log("Monitoring...");
+
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+
+      //console.log(characteristic?.uuid);
+      //console.log(characteristic?.value);
+      const decodedValue = base64.decode(characteristic?.value);
+      console.log(decodedValue);
+      setNotifChar(characteristic);
     });
   }
 
@@ -123,6 +142,8 @@ const App = () => {
       <Button title="Connect" onPress={() => connectToDevice()} />
       <Button title="Write" onPress={() => writeToDevice("hello")} />
       <Button title="Read" onPress={() => readFromDevice()} />
+      <Button title="Subscribe" onPress={() => subscribeToDevice()} />
+      <Text style={{fontSize: 30}}>Notif: {base64.decode(notifChar?.value)}</Text>
     </SafeAreaView>
   );
 };
