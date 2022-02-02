47449c27ae098f42405553b0d95efc5143621860
frknilisu <frkn.ilisu@gmail.com>
2022-02-02 07:50:32 +0100

    add write to device functionality

--- App.tsx
+++ App.tsx
@@ -33,12 +33,15 @@ LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by mes
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 import { BleManager, Device } from 'react-native-ble-plx';
+import base64 from 'react-native-base64';
 
 const App = () => {
   
   const bleManager: BleManager = new BleManager();
   const [myDevice, setMyDevice] = useState<Device>();
   
+  const SERVICE_UUID = "a5b1194c-0d11-e0c0-50e5-c7db856efe37";
+  const CHAR_UUID= "98234604-4499-8e00-79d3-44a0c5481593";
 
   const scanDevices = (deviceName: String) => {
     bleManager.startDeviceScan(null, null, (error, device) => {
@@ -78,6 +81,11 @@ const App = () => {
                 console.log(services.length);
                 services.map((service, i) => {
                   console.log(service.id, service.uuid);
+                  service.characteristics().then(characteristics => {
+                    characteristics.map((char, j) => {
+                      console.log(char.id, char.uuid);
+                    });
+                  });
                 });
               });
             });
@@ -86,11 +94,24 @@ const App = () => {
       });
   }
 
+  const writeToDevice = (data: any) => {
+    console.log("Is Connected: ", myDevice.isConnected());
+    bleManager.writeCharacteristicWithResponseForDevice(
+      myDevice.id, SERVICE_UUID, CHAR_UUID, base64.encode(data).toString('utf-8')
+    ).then(response => {
+      console.log(response);
+    }).catch((error) => {
+      console.log('error in writing data');
+      console.log(error);
+    });
+  }
+
   return (
     <SafeAreaView>
       <Text style={styles.highlight}>Highlighted Text</Text>
       <Button title="Scan" onPress={() => scanDevices("MyBLEServer")} />
       <Button title="Connect" onPress={() => connectToDevice()} />
+      <Button title="Write" onPress={() => writeToDevice("hello")} />
     </SafeAreaView>
   );
 };
